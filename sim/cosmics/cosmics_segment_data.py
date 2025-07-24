dbba6a45b0b5df5e2162543c963702b8c1ed2b87
John William Vaccaro <jvaccaro@sdfmilan075.sdf.slac.stanford.edu>
2025-07-24 14:30:09 -0700

    Add timing information

--- sim/cosmics/cosmics_segment_data.py
+++ sim/cosmics/cosmics_segment_data.py
@@ -6,6 +6,8 @@ infile_dir = '/sdf/data/neutrino/yuntse/coherent/SNeNDSens/g4/Cosmics'
 outfile_dir = 'graph_data'
     
 if __name__ == "__main__":
+    data_prenergies = []
+    data_prlengths = []
     data_lctes = []
     data_lctls = []
     edata_particles = {}
@@ -28,7 +30,7 @@ if __name__ == "__main__":
         temp_totalenergy = [0, 0, 0]
 
         for seg in f['segments']:
-            if seg['event_id'] > event_id:
+            if seg['event_id'] > event_id:             
                 greatest_energy_dep = 0
                 longest_path = 0
 
@@ -41,7 +43,7 @@ if __name__ == "__main__":
                         longest_path = traj_id
 
                 # Save the longest path and highest energy deposit (in active volume), as well as the particles responsible
-                if temp_energies[greatest_energy_dep] > 0:
+                if temp_energies[greatest_energy_dep] > 0:                   
                     data_lctes.append(temp_energies[greatest_energy_dep])
                     data_lctls.append(temp_lengths[greatest_energy_dep])
                     if (edata_particles.get(temp_particles[greatest_energy_dep])):
@@ -57,6 +59,14 @@ if __name__ == "__main__":
                         ldata_particles[temp_particles[longest_path]] = 1
                 
                 if temp_totalenergy[2] > 0:
+                    # Save primary muon trajectory energy and length
+                    if 0 in temp_particles:
+                        data_prenergies.append(temp_energies[0])
+                        data_prlengths.append(temp_lengths[0])
+                    else:
+                        data_prenergies.append(0)
+                        data_prlengths.append(0)
+                    
                     data_fenergy.append(temp_totalenergy[0])
                     data_senergy.append(temp_totalenergy[1])
                     data_aenergy.append(temp_totalenergy[2])
@@ -70,40 +80,38 @@ if __name__ == "__main__":
                 
                 event_id = seg['event_id']
 
+            # Save the distance traveled inside the active, signal, and fiducial volumes
+            in_active = get_length_in_active_volume(seg['x_start'], seg['x_end'], 
+                                                    seg['y_start'], seg['y_end'], 
+                                                    seg['z_start'], seg['z_end'])
+            in_signal = get_length_in_signal_volume(seg['x_start'], seg['x_end'], 
+                                                    seg['y_start'], seg['y_end'], 
+                                                    seg['z_start'], seg['z_end'])
+            in_fiducial = get_length_in_fiducial_volume(seg['x_start'], seg['x_end'], 
+                                                        seg['y_start'], seg['y_end'], 
+                                                        seg['z_start'], seg['z_end'])
+
             # Add the energy deposited in the active, signal, and fiducial volumes for this segment
-            temp_totalenergy[0] += seg['dEdx'] * get_length_in_fiducial_volume(seg['x_start'], seg['x_end'], 
-                                                                               seg['y_start'], seg['y_end'], 
-                                                                               seg['z_start'], seg['z_end'])
+            temp_totalenergy[0] += seg['dEdx'] * in_fiducial
 
-            temp_totalenergy[1] += seg['dEdx'] * get_length_in_signal_volume(seg['x_start'], seg['x_end'], 
-                                                                             seg['y_start'], seg['y_end'], 
-                                                                             seg['z_start'], seg['z_end'])
+            temp_totalenergy[1] += seg['dEdx'] * in_signal
 
-            temp_totalenergy[2] += seg['dEdx'] * get_length_in_active_volume(seg['x_start'], seg['x_end'], 
-                                                                             seg['y_start'], seg['y_end'], 
-                                                                             seg['z_start'], seg['z_end'])
+            temp_totalenergy[2] += seg['dEdx'] * in_active
 
             # Add the energy deposited and segment length (in active volume) to determine the trajectory with the highest energy deposition and longest path
             if temp_particles.get(seg['traj_id']):
-                temp_energies[seg['traj_id']] += seg['dEdx'] * get_length_in_active_volume(seg['x_start'], seg['x_end'], 
-                                                                                           seg['y_start'], seg['y_end'], 
-                                                                                           seg['z_start'], seg['z_end'])
-                temp_lengths[seg['traj_id']] += get_length_in_active_volume(seg['x_start'], seg['x_end'], 
-                                                                            seg['y_start'], seg['y_end'], 
-                                                                            seg['z_start'], seg['z_end'])
+                temp_energies[seg['traj_id']] += seg['dEdx'] * in_active
+                temp_lengths[seg['traj_id']] += in_active
             else:
                 temp_particles[seg['traj_id']] = seg['pdg_id']
-                temp_energies[seg['traj_id']] = seg['dEdx'] * get_length_in_active_volume(seg['x_start'], seg['x_end'], 
-                                                                                          seg['y_start'], seg['y_end'], 
-                                                                                          seg['z_start'], seg['z_end'])
-                temp_lengths[seg['traj_id']] = get_length_in_active_volume(seg['x_start'], seg['x_end'], 
-                                                                                          seg['y_start'], seg['y_end'], 
-                                                                                          seg['z_start'], seg['z_end'])
+                temp_energies[seg['traj_id']] = seg['dEdx'] * in_active
+                temp_lengths[seg['traj_id']] = in_active
 
     print("Writing to output...")
-    np.savez_compressed(outfile_dir + '/cosmics_segment_data.npz', fenergy=data_fenergy, senergy=data_senergy, aenergy=data_aenergy, 
-                        pfenergy=data_pfenergy, psenergy=data_psenergy, lctes=data_lctes, lctls=data_lctls, lengths=data_lengths, 
-                        eparticles=np.array(list(edata_particles.keys())), ecounts=np.array(list(edata_particles.values())),
-                        lparticles=np.array(list(ldata_particles.keys())), lcounts=np.array(list(ldata_particles.values())))
+    np.savez_compressed(outfile_dir + '/cosmics_segment_data.npz', fenergy=data_fenergy, senergy=data_senergy, aenergy=data_aenergy,
+                        pfenergy=data_pfenergy, psenergy=data_psenergy, prenergies=data_prenergies, prlengths=data_prlengths, lctes=data_lctes,
+                        lctls=data_lctls, lengths=data_lengths, eparticles=np.array(list(edata_particles.keys())),
+                        ecounts=np.array(list(edata_particles.values())), lparticles=np.array(list(ldata_particles.keys())),
+                        lcounts=np.array(list(ldata_particles.values())))
     
     print("Data successfully written to file cosmics_segment_data.npz!")
